52ac6d2bbc9e0c341320063629029c9c8608915c
yokurin <tsubasa@hayashitsubasa-no-MacBook-Pro.local>
2014-06-13 11:24:19 +0900

    1st 20140613_php

---
+++ form/form_test.php
@@ -0,0 +1,1 @@
+<?php print ('お名前： '. htmlspecialchars($_REQUEST['my_name'],ENT_QUOTES)); ?> 
---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>PHP Test</title>
+	</head>
+
+	<body>
+		<?php print (date('H時i分s秒')); ?> 
+	</body>
+</html>
