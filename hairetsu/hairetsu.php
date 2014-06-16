8e408ac3abe5a3315cad0c279041df7788eb70b4
yokurin <tsubasa@hayashitsubasa-no-MacBook-Pro.local>
2014-06-16 10:01:46 +0900

    no message

--- hairetsu/hairetsu.php
+++ hairetsu/hairetsu.php
@@ -1,29 +1,18 @@
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>PHP Test</title>
-	</head>
+<form action="" method="get">
+	<dl>
+		<dt>商品</dt>
+			<dd>
 
-	<body>
-		<form action="" method="get">
-			<dl>
-				<dt>商品</dt>
-				<dd>
+				<?php
+					&items = array('a-1' => 'ガム', 'b-1' => 'チョコレート', 'c-3' => 'クッキー');
 
-					<?php
-						&items = array('a-1' => 'ガム', 'b-1' => 'チョコレート', 'c-3' => 'クッキー');
-
-						foreach ($items as $itemKey => $itemValue) 
-						{
-
-							print('<input type="checkbox" id="'.$itemKey.'" value="'.$itemKey.'"/>
+					foreach ($items as $itemKey => $itemValue) 
+					{
+						print('<input type="checkbox" id="'.$itemKey.'" value="'.$itemKey.'"/>
 									<label for="'.$itemKey.'">'.$itemValue.'</label>');
-						};
-					?> 
-				</dd>
-			</dl>
-		<input type="submit" value="送信する" />
-		</form>
-	</body>
-
-</html>
+					}
+				?> 
+			</dd>
+	</dl>
+	<input type="submit" value="送信する" />
+</form>
